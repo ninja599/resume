1f0da2672daa5199d5b01bb90ca7691cfbe7804a
Erik Stenmark <erik.stenmark@gmai.com>
2018-08-01 16:30:53 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,206 @@
+<!doctype html>
+
+<head>
+	<meta charset="utf-8">
+	<title>CV - Erik Stenmark</title>
+	<meta name="description" content="The HTML5 Herald">
+	<meta name="author" content="SitePoint">
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Mono:400,300,300italic,700,500,500italic,700italic,400italic%7CLato:400,700,300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<div id="loader-wrapper">
+
+	<div id="loader"></div>
+	
+	<div class="loader-section section-left"></div>
+	<div class="loader-section section-right"></div>
+	
+</div>
+
+<div id="wrapper">
+<span id="uplink"><a href="#header"><img src="img/up.png"></a></span>
+<main> 
+<div class="full-width">
+	<header class="header" id="header" style="background-image: url('img/banner.jpg')">
+		<div class="sector">
+			<h1 class="main-title"> Erik Stenmark </h1><!-- /. name -->
+			<h4 class="tagline"> Web developer </h4><!-- /. profession -->
+		</div>
+		<div class="profimg" style="background-image: url('img/profile.jpg')"></div>
+		<div class="menu social">
+			<ul>
+				<li><a target="_blank" href="https://www.linkedin.com/in/erikstenmark/"><img src="img/in.png"></a></li>
+				<li><a target="_blank" href="https://www.facebook.com/erik.stenmark.98"><img src="img/fb.png"></a></li>
+				<li><a target="_blank" href="https://github.com/ninja599"><img src="img/gh.png"></a></li>
+			</ul>
+		</div>
+	</header> <!-- /. header/cover -->
+	<div class="menu navigation">
+		<ul>
+			<li><a href="#about">Tietoa minusta</a></li>
+			<li><a href="#experience">Kokemus</a></li>
+			<li><a href="#skills">Taidot</a></li>
+			<li><a href="#portfolio">Työnäytteet</a></li>
+			<li><a href="#contact">Yhteystiedot</a></li>
+		</ul>
+	</div>
+</div>
+
+<section class="content">
+	<div id="about">
+		<p class="sectiontitle">Tietoa minusta</p>
+		<div class="box wide">
+			<img class="icon float-right" src="img/about.png">
+			<p>Olen Vaasassa asuva majoitusalan aluepäällikkö jonka intohimo on jo 
+      lukuisten vuosien ajan ollut ohjelmointi ja tämän alan opiskelu. </p>
+      
+      <p>Tilanteeni tällä hetkellä on se, että olen täysin itse oppinut 
+      ohjelmoinnin harrastelija vailla työkokemusta. Vapaa-aikani olen käyttänyt 
+      vuoden 2017 alusta lähtien aktiiviseen itsetoimiseen opiskeluun eri digitaalisten 
+      portaaleiden avulla esim. Team treehouse, Codeacademy, Stack owerflow ja Youtube. 
+      Koen että olen päässyt oppimisessani siihen pisteeseen, että olisin vailla 
+      tehokkaampaa syventämistä osaamiselleni jota tarjoaisi parhaiten työn 
+      kautta ansaittu käytännön kokemus. </p>
+		</div>
+		
+		<div class="box narrow">
+			<ul>
+				<li>Erik Stenmark</li>
+				<li>Est. 8.5.1985</li>
+        <li>Kielitatio:
+          <p>Ruotsi: Äidinkieli<p>
+          <p>Suomi: Hyvä<p>
+          <p>Englanti: Hyvä<p>
+        </li>
+			</ul>
+		</div>
+	<div class="clearfix"></div>
+	</div>
+	
+	
+	<div id="experience">
+	<p class="sectiontitle">Kokemus</p>
+		<div class="box half">
+			<p>Ohjelmoinnin kokemusta minulla ei toistaiseksi ole työn puolesta. 
+         Tämän sijan olen vapaa-ajallani rakennellut harjoittelumielessä sovelluksia 
+         eri ohjelmointikieliä hyödyntämällä. Koen että vahvinta osaamiseni on 
+         Web-ohjelmoinnin puolella, mutta olen toki osittain perehtynyt myös työpöytä 
+         sovellusten kehittämiseen Java kielellä. Viimeisimpiä projektejani ovat mm. 
+         Varauskalenteri suvun vapaa-ajan asunnolle PHP/JavaScript keiltä ja MySQL 
+         tietokantaa käyttämällä. Lisäksi rakennan kauppalista sovellusta muuten samoilla 
+         kielillä mutta tietokannaksi olen tähän sovellukseen valinnut SQLite3. Näihin 
+         projekteihin pääsee osittain perehtymään GitHub profiilillani.</p>
+			<img class="icon float-right" src="img/exp.png">
+		</div>
+		
+		<div class="box half">
+      <h1>Viimeisin työkokemus:</h1>
+      <ul>
+        <li>Majoituspalvelu Forenom Oy<p>30.10.2017 -></p><p>Aluemyyntipäällikkö</p></li>
+        <li>Osuuspankki, Vaasa <p>4.5.2015 – 27.10.2017</p><p>Sijoitusneuvoja</p></li>
+        <li>Gigantti Oy, Vaasa <p>14.8.2012 – 1.5.2015</p><p>Sijaisosastopäällikkö</p></li>
+      </ul>
+      <br><br>
+      <p>Täysi CV ladattavana <a href="files/cv.pdf">tästä</a></p>
+		</div>
+	<div class="clearfix"></div>
+	</div>
+	
+	
+	<div id="skills">
+		<p class="sectiontitle">Taidot</p>
+		<div class="box narrow">
+			<img class="icon float-right" src="img/skills.png">
+      <p>HTML ja CSS kieltä olen käyttänyt jo hyvin nuoresta pitäen 
+      ja viime vuosina olen perehtynyt hyvinkin paljon PHP sekä 
+      JavaScript kieliin. Tietokannat kuten MySQL ja SQLite3 ovat 
+      myös hyvinkin tuttuja minulle ja hyödynnän näitä tekniikoita 
+      jatkuvasti eri projekteissa.</p>
+      <p>Kielet: HTML, CSS, JavaScript, PHP, Python, Java</p>
+      <p>Tietokannat: MySQL, SQLite3</p>
+      <p>Ohjelmisto: Linux, Git, Photoshop, Notepad++, Visual Studio, IntelliJ IDEA, SSH sovellukset, Apache2, Xampp, Mamp
+    </div>
+		
+		<div class="box wide">
+			<div class="progbars">
+        <div class="progressbar">
+          <div class="title htmlcolor">HTML5</div>
+          <div class="progress html"></div>
+          <div class="progressvalue html" value="85"></div>
+        </div>
+        <div class="progressbar">
+          <div class="title csscolor">CSS</div>
+          <div class="progress css"></div>
+          <div class="progressvalue css" value="50"></div>
+        </div>
+        <div class="progressbar">
+          <div class="title jscolor">JavaScript</div>
+          <div class="progress js"></div>
+          <div class="progressvalue js" value="45"></div>
+        </div>
+        <div class="progressbar">
+          <div class="title phpcolor">PHP</div>
+          <div class="progress php"></div>
+          <div class="progressvalue php" value="70"></div>
+        </div>
+        <div class="progressbar">
+          <div class="title ajaxcolor">AJAX</div>
+          <div class="progress ajax"></div>
+          <div class="progressvalue ajax" value="40"></div>
+        </div>
+      </div>
+      <p>Ohjelmointikielten oppimiseni suhteen olen tehnyt sen linjauksen, 
+      että uutta kieltä oppiessani panostan aluksi kielen perusymmärrykseen 
+      ja järkevien suunnittelu mallien omaksumiseen sivuttamalla usein lukuisat 
+      saatavilla olevat sovelluskehykset kyseiselle kielelle. PHP:n Composer 
+      ja Node.js:n NPM sovellukset ovat kuitenkin tuttuja käsitteitä minulle. 
+      Tämän lisäksi olen perehtynyt jQuery ja React kehyksiin alustavasti.</p>
+		</div>
+	<div class="clearfix"></div>
+	</div>
+	
+	
+	<div id="portfolio">
+	<p class="sectiontitle">Portfolio</p>
+		<div class="box full">
+			<img class="icon float-left" src="img/portfolio.png">
+			<p>Beast. Meat set greater she'd creeping from were under bearing won't wherein it open. Light. From midst all fill saying day Very void land their fly. 
+			Heaven green without whose replenish seasons there brought, over own sixth in Beginning. </p>
+		</div>
+	<div class="clearfix"></div>	
+	</div>
+	
+	
+	<div id="contact">
+	<p class="sectiontitle">Contact</p>
+		<div class="box wide">
+			<img class="icon float-right" src="img/contact.png">
+			<p>Otathan yhteyttä mikäli yritykselläsi olisi tarvetta nopasti kehittyvälle ja intohimoiselle koodari.</p>
+		</div>
+		
+		<div class="box narrow">
+      <ul>
+				<li>050 0564035</li>
+				<li>Klemetinkatu 16 as 1, 65100, Vaasa</li>
+				<li><a href="demo.stenmark.eu">demo.stenmark.eu</a></li>
+				<li><a href="mailto:erik.stenmark@gmail.com" target="_top">erik.stenmark@gmail.com</a></li>
+      </ul>
+		</div>
+	<div class="clearfix"></div>
+	</div>
+	
+</section>
+
+</main>
+
+<footer class="footer">
+<p>&copy;<?php echo date('Y')?> Erik Stenmark</p>
+</footer>
+
+</div> <!-- end of wrapper -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
---
+++ js/scripts.js
@@ -0,0 +1,99 @@
+document.addEventListener('DOMContentLoaded', () => { setTimeout(() => { document.querySelector('body').className = 'loaded';}, 10)}, false);
+
+let width = 10;
+
+
+const htmlp = document.getElementsByClassName("html")[1].getAttribute('value');
+const cssp = document.getElementsByClassName("css")[1].getAttribute('value');
+const jsp = document.getElementsByClassName("js")[1].getAttribute('value');
+const phpp = document.getElementsByClassName("php")[1].getAttribute('value');
+const ajaxp = document.getElementsByClassName("ajax")[1].getAttribute('value');
+const uplink = document.getElementById("uplink");
+const skillspos = document.getElementById("skills").offsetTop;
+
+const prog = {
+	html:htmlp,
+	css:cssp,
+	js:jsp,
+	php:phpp,
+	ajax:ajaxp
+}
+
+
+function scrollact(){
+	console.log(window.pageYOffset);
+	if(window.pageYOffset > skillspos - 100) {
+	for(var propt in prog){
+		let width = prog[propt];
+		setTimeout(move(propt, width), 0);
+	}
+}
+    if(window.pageYOffset>50){
+		uplink.style.display = "inline";
+		let op = window.pageYOffset/2500;
+		if(op < 0.5){
+			uplink.style.opacity = op;
+		} else {
+			uplink.style.opacity = 0.5;
+		}
+	} else {
+		uplink.style.display = "none";
+	}
+}
+
+
+function round5(x) {
+    return Math.ceil(x/5)*5;
+}
+
+const move = (lang, prog) => {
+	let skill = document.getElementsByClassName(lang)[0];
+	let pvalue = document.getElementsByClassName(lang)[1];
+	const id = setInterval(frame, 100);
+	function frame() {
+		if (width >= prog) {
+		  clearInterval(id);
+		} else {
+		  width++; 
+		  skill.style.width = width + '%'; 
+		  pvalue.innerHTML = round5(width) + '%';
+		}
+	}
+}
+
+window.onscroll=scrollact
+
+document.addEventListener('wheel',function (event){
+  if (event.deltaY > 0) {
+    event.preventDefault();
+    smoothScroll(document.documentElement,100,100)
+  }
+})
+
+function smoothScroll (domElement,pixel,delay) {
+  const intervalToRepeat = 15;
+  const step = (intervalToRepeat * pixel) / delay;
+  if ( step < pixel) {
+    domElement.scrollTop += step;
+    setTimeout(function (){
+      smoothScroll(domElement,pixel - step,delay)
+    },intervalToRepeat);
+  }
+}
+
+$(document).ready(function(){
+	const scrollOffset = 40;
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - scrollOffset
+      }, 800, function(){
+
+        window.location.hash = hash - scrollOffset;
+      });
+    }
+  });
+});
