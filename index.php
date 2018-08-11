51c937f5303aab140e4258cabc00d8dfa6e57b02
Erik Stenmark <erik.stenmark@gmai.com>
2018-08-11 15:28:15 +0300

    Added portfolio

--- index.php
+++ index.php
@@ -45,6 +45,7 @@ include 'inc/log.php';
         <li><a href="#about">Tietoa minusta</a></li>
         <li><a href="#experience">Kokemus</a></li>
         <li><a href="#skills">Taidot</a></li>
+        <li><a href="#portfolio">Työnäytteet</a></li>
         <li><a href="#contact">Yhteystiedot</a></li>
       </ul>
     </div>
@@ -66,47 +67,47 @@ include 'inc/log.php';
         olisi työssä oppiminen.</p>
       </div>
       
-      <div class="box narrow">
+      <div class="box narrow ul">
         <ul>
           <li>Erik Stenmark</li>
           <li>8.5.1985</li>
           <li>Kielitaito:
-            <p>Ruotsi: Äidinkieli<p>
-            <p>Suomi: Erinomainen<p>
-            <p>Englanti: Erinomainen<p>
+            <p>Ruotsi: Äidinkieli</p>
+            <p>Suomi: Erinomainen</p>
+            <p>Englanti: Erinomainen</p>
           </li>
         </ul>
       </div>
-    <div class="clearfix"></div>
+      <div class="clearfix"></div>
     </div>
     
     
     <div id="experience">
-    <p class="sectiontitle">Kokemus</p>
-      <div class="box half">
-        <p>Toistaiseksi minulla ei ole ammatin kautta hankittua ohjelmoinnin kokemusta. 
-           Sen sijan olen vapaa-ajallani rakennellut harjoittelumielessä sovelluksia 
-           lukuisia ohjelmointikieliä hyödyntämällä. Koen vahvimman osaamiseni olevan 
-           Web-ohjelmoinnin puolella, mutta olen toki osittain perehtynyt myös työpöytä 
-           sovellusten kehittämiseen Java kielellä. Viimeisimpiä projektejani ovat
-           Varauskalenteri vapaa-ajan asunnolle PHP/JavaScript-kieltä ja MySQL 
-           tietokantaa käyttämällä. Lisäksi rakennan lista sovellusta samoilla 
-           kielillä. Tietokannaksi olen tähän sovellukseen valinnut SQLite3.
-           Projekteihin pääsee osittain perehtymään GitHub profiilillani.</p>
-        <img class="icon float-right" src="img/exp.png">
-      </div>
-      
-      <div class="box half">
-        <h1>Viimeisin työkokemus:</h1>
-        <ul>
-          <li>Majoituspalvelu Forenom Oy<p>30.10.2017 -></p><p>Aluemyyntipäällikkö</p></li>
-          <li>Osuuspankki, Vaasa <p>4.5.2015 – 27.10.2017</p><p>Sijoitusneuvoja</p></li>
-          <li>Gigantti Oy, Vaasa <p>14.8.2012 – 1.5.2015</p><p>Sijaisosastopäällikkö</p></li>
-        </ul>
-        <br><br>
-        <p>CV ladattavana <a href="files/cv.pdf">tästä</a></p>
-      </div>
-    <div class="clearfix"></div>
+      <p class="sectiontitle">Kokemus</p>
+        <div class="box half">
+          <p>Toistaiseksi minulla ei ole ammatin kautta hankittua ohjelmoinnin kokemusta. 
+             Sen sijan olen vapaa-ajallani rakennellut harjoittelumielessä sovelluksia 
+             lukuisia ohjelmointikieliä hyödyntämällä. Koen vahvimman osaamiseni olevan 
+             Web-ohjelmoinnin puolella, mutta olen toki osittain perehtynyt myös työpöytä 
+             sovellusten kehittämiseen Java kielellä. Viimeisimpiä projektejani ovat
+             Varauskalenteri vapaa-ajan asunnolle PHP/JavaScript-kieltä ja MySQL 
+             tietokantaa käyttämällä. Lisäksi rakennan lista sovellusta samoilla 
+             kielillä. Tietokannaksi olen tähän sovellukseen valinnut SQLite3.
+             Projekteihin pääsee osittain perehtymään GitHub profiilillani.</p>
+          <img class="icon float-right" src="img/exp.png">
+        </div>
+        
+        <div class="box half ul">
+          <h1>Viimeisin työkokemus:</h1>
+          <ul>
+            <li>Majoituspalvelu Forenom Oy<p>30.10.2017 -></p><p>Aluemyyntipäällikkö</p></li>
+            <li>Osuuspankki, Vaasa <p>4.5.2015 – 27.10.2017</p><p>Sijoitusneuvoja</p></li>
+            <li>Gigantti Oy, Vaasa <p>14.8.2012 – 1.5.2015</p><p>Sijaisosastopäällikkö</p></li>
+          </ul>
+          <br><br>
+          <p>CV ladattavana <a href="files/cv.pdf">tästä</a></p>
+        </div>
+      <div class="clearfix"></div>
     </div>
     
     
@@ -160,15 +161,26 @@ include 'inc/log.php';
     <div class="clearfix"></div>
     </div>
     
-    
-    <div id="contact">
-    <p class="sectiontitle">Yhteystiedot</p>
-      <div class="box wide">
-        <img class="icon float-right" src="img/contact.png">
-        <p>Mikäli yritykselläsi olisi tarvetta nopasti kehittyvälle ja intohimoiselle koodari, niin otathan ihmeessä yhteyttä.</p>
+    <div id="portfolio">
+      <p class="sectiontitle">Työnäytteet</p>
+      <div id="drag-scroll" class="box wide">
+        <ul></ul>
       </div>
       
-      <div class="box narrow">
+      <div class="box narrow" style="height: 240px" id="siteinfo">
+        <h1 class="sitetitle"></h1>
+        <p class="sitelangs"></p>
+        <p class="sitedesc"></p>
+      </div>
+      
+      <div class="clearfix"></div>
+    </div>
+    
+    
+    <div id="contact">
+      <p class="sectiontitle">Yhteystiedot</p>
+        
+      <div class="box narrow ul">
         <ul>
           <li>050 0564035</li>
           <li>Klemetinkatu 16 as 1, 65100, Vaasa</li>
@@ -176,9 +188,14 @@ include 'inc/log.php';
           <li><a href="mailto:erik.stenmark@gmail.com" target="_top">erik.stenmark@gmail.com</a></li>
         </ul>
       </div>
-    <div class="clearfix"></div>
-    </div>
+      
+      <div class="box wide">
+        <img class="icon float-right" src="img/contact.png">
+        <p>Mikäli yritykselläsi olisi tarvetta nopasti kehittyvälle ja intohimoiselle koodari, niin otathan ihmeessä yhteyttä.</p>
+      </div>
     
+      <div class="clearfix"></div>
+    </div>
   </section>
 
   </main>
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => { setTimeout(() => { document.querySelector('body').className = 'loaded';}, 1500)}, false);
+document.addEventListener('DOMContentLoaded', () => { setTimeout(() => { document.querySelector('body').className = 'loaded';}, 0)}, false);
 
 let width = 10;
 
@@ -62,24 +62,6 @@ const move = (lang, prog) => {
 
 window.onscroll=scrollact
 
-document.addEventListener('wheel',function (event){
-  if (event.deltaY > 0) {
-    event.preventDefault();
-    smoothScroll(document.documentElement,100,100)
-  }
-})
-
-function smoothScroll (domElement,pixel,delay) {
-  const intervalToRepeat = 15;
-  const step = (intervalToRepeat * pixel) / delay;
-  if ( step < pixel) {
-    domElement.scrollTop += step;
-    setTimeout(function (){
-      smoothScroll(domElement,pixel - step,delay)
-    },intervalToRepeat);
-  }
-}
-
 $(document).ready(function(){
 	const scrollOffset = 40;
   $("a").on('click', function(event) {
@@ -96,3 +78,130 @@ $(document).ready(function(){
     }
   });
 });
+ 
+ 
+  
+const body = document.querySelector('body');
+const dragDiv = body.querySelector('div#drag-scroll');
+const dragUl = dragDiv.querySelector('ul');
+
+const demoSiteInfoDiv = body.querySelector('div#siteinfo');
+const demoSiteTitle = demoSiteInfoDiv.querySelector('h1.sitetitle');
+const demoSiteLangs = demoSiteInfoDiv.querySelector('p.sitelangs');
+const demoSiteDesc = demoSiteInfoDiv.querySelector('p.sitedesc');
+
+function ajax(action, arg) {
+  var hr = new XMLHttpRequest();
+  var url = "http://demo.stenmark.eu/sites/sitesapi.php";
+  if (action == 'allsites') { var vars = "allsites";}
+  hr.open("POST", url, true);
+  hr.withCredentials = true;
+  hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  hr.onreadystatechange = function() {
+    if(hr.readyState == 4 && hr.status == 200) {
+      var return_data = hr.responseText;
+      // Ajax returns
+      if (action == 'allsites') {
+        display_sites(return_data);
+      }
+    }
+  }
+  hr.send(vars);
+}
+
+function display_sites(json) {
+  sitesObj = JSON.parse(json);
+  var sites = '';
+  for (var i = 0; i<sitesObj.length; i++) {
+    sites += '<li>';
+    sites += '<div class="demo-site" id="'+sitesObj[i].dir+'" style="background-image: url('+sitesObj[i].img+')">';
+    sites += '<h1 class="a-reveal">'+sitesObj[i].title+'</h1>';
+    sites += '<p class="a-reveal">'+sitesObj[i].descs.fi.short+'</p>';
+    sites += '<div class="site-info-link">';
+    sites += '<a class="a-reveal">info</a>';
+    sites += '</div>';
+    sites += '</div>';
+    sites += '</li>';
+  }
+  dragUl.innerHTML = sites;
+  
+  var lis = dragUl.children;
+  
+  for (let i = 0; i<lis.length; i++) {
+    lis[i].addEventListener('click', (e) => {
+      if(e.target.tagName == 'H1') {
+        window.location.href = sitesObj[i].url;
+      } else if (e.target.tagName == 'A') {
+        demoSiteTitle.innerHTML = 
+          '<a href="'+sitesObj[i].url+'">'+
+            sitesObj[i].title;
+          +'</a>';
+          
+        demoSiteLangs.textContent = sitesObj[i].languages.toString();
+        demoSiteDesc.textContent = sitesObj[i].descs.fi.long;
+        demoSiteInfoDiv.classList.add('display-block');
+      }
+    });
+  }
+}
+
+ajax('allsites', null);
+
+
+dragUl.addEventListener('wheel', (e) => {
+  if (e.deltaY < 0) {
+    e.preventDefault();
+    dragUl.scrollLeft -= 50;
+  }
+  if (e.deltaY > 0) {
+    e.preventDefault();
+    dragUl.scrollLeft += 50;
+  }
+});
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+dragUl.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  isDown = true;
+  dragUl.classList.add('active')
+  startX = e.pageX - dragUl.offsetLeft;
+  scrollLeft = dragUl.scrollLeft;
+});
+dragUl.addEventListener('mouseleave', (e) => {
+  isDown = false;
+  dragUl.classList.remove('active')
+});
+dragUl.addEventListener('mouseup', (e) => {
+  isDown = false;
+  dragUl.classList.remove('active')
+});
+dragUl.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - dragUl.offsetLeft;
+  const walk = (x - startX) * 2;
+  dragUl.scrollLeft = scrollLeft - walk;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+  
